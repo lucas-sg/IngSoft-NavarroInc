d295036ddf20143bf21212f2129b300f1487e833
navi <rnavarro@itba.edu.ar>
2017-10-05 14:44:56 -0300

    Clase user agregada

---
+++ src/User.java
@@ -0,0 +1,20 @@
+
+
+
+public class User {
+    private String name;
+    private String password;
+    private String surname;
+    private String eMail;
+    private String country;
+    private String province;
+    private String city;
+    private String street;
+    private Integer number;
+    private Integer zipCode;
+    private String telephone;
+    private String docNumber;
+    private enum docType{ DNI,PASSPORT}
+    private enum telephoneType{}
+
+}
